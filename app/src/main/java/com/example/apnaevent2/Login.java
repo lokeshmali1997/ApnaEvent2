34309eee7b1ff8e20ef22e3e337594bbc6661d7b
Ravi Kumar <ravibhati786@gmail.com>
2020-03-26 18:46:44 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml

--- app/src/main/java/com/example/apnaevent2/Login.java
+++ app/src/main/java/com/example/apnaevent2/Login.java
@@ -1,11 +1,31 @@
 package com.example.apnaevent2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.data.DataBufferObserverSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+
+    EditText etUsername,etPassword;
+    Button btnLogin;
+
+    DatabaseReference mUserMaster = FirebaseDatabase.getInstance().getReference().child("UserMaster");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +34,61 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+
+        etUsername  = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mUserMaster.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot data : dataSnapshot.getChildren()){
+                            if(data.child("email").getValue().equals(etUsername.getText().toString()) ||
+                                    data.child("phone").getValue().equals(etUsername.getText().toString()))
+                            {
+                                if(data.child("pass").getValue().equals(etPassword.getText().toString()))
+                                {
+                                    Toast.makeText(Login.this, data.getKey().toString(), Toast.LENGTH_SHORT).show();
+                                    sharedUserLogin(data.getKey().toString());
+                                }
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+            SharedPreferences sh =  getSharedPreferences("UserData",MODE_PRIVATE);
+
+            if(sh.contains("UserId"))
+            {
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+            }
+
+    }
+
+    private void sharedUserLogin(String id)
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences("UserData",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+        myEdit.putString("UserId",id);
+        myEdit.commit();
     }
 }
