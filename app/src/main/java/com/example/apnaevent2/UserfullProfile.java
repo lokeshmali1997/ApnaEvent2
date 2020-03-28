d555fd1564375f06c78fe5b94bfee9c3b062b881
lokesh <lokeshmali1997@gmail.com>
2020-03-28 18:29:10 +0530

    home commit

--- app/src/main/java/com/example/apnaevent2/UserfullProfile.java
+++ app/src/main/java/com/example/apnaevent2/UserfullProfile.java
@@ -12,7 +12,8 @@ import android.widget.TextView;
 
 public class UserfullProfile extends AppCompatActivity {
 
-
+    TextView textView,textView1,textView2,textView3,textView4,textView5,textView6,textView7;
+    Button etbtn,savebtn,btnlogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,19 +46,19 @@ public class UserfullProfile extends AppCompatActivity {
 
     public  void editprofile(View view){
 
-        Button etbtn = findViewById(R.id.btnedit);
-        Button savebtn = findViewById(R.id.btnsave);
-        Button btnlogout= findViewById(R.id.btnlogout);
+        etbtn = findViewById(R.id.btnedit);
+        savebtn = findViewById(R.id.btnsave);
+        btnlogout= findViewById(R.id.btnlogout);
 
 
-        TextView textView = findViewById(R.id.txtusername);
-        TextView textView1 = findViewById(R.id.txtemail);
-        TextView textView2 = findViewById(R.id.txtpassword);
-        TextView textView3 = findViewById(R.id.txtnumbaer);
-        TextView textView4 = findViewById(R.id.etUser);
-        TextView textView5 = findViewById(R.id.etEmail);
-        TextView textView6 = findViewById(R.id.etPass);
-        TextView textView7 = findViewById(R.id.etPhone);
+         textView = findViewById(R.id.txtusername);
+         textView1 = findViewById(R.id.txtemail);
+         textView2 = findViewById(R.id.txtpassword);
+         textView3 = findViewById(R.id.txtnumbaer);
+         textView4 = findViewById(R.id.etUser);
+         textView5 = findViewById(R.id.etEmail);
+         textView6 = findViewById(R.id.etPass);
+         textView7 = findViewById(R.id.etPhone);
 
 
         etbtn.setOnClickListener(new View.OnClickListener() {
@@ -87,15 +88,30 @@ public class UserfullProfile extends AppCompatActivity {
 
     public void saveprofile(View view) {
 
-        Button savebtn = findViewById(R.id.btnsave);
-        Button etbtn = findViewById(R.id.btnedit);
+         savebtn = findViewById(R.id.btnsave);
+         etbtn = findViewById(R.id.btnedit);
 
 
         savebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
                 savebtn.setVisibility(view.INVISIBLE);
+                btnlogout.setVisibility(view.VISIBLE);
+                etbtn.setVisibility(view.INVISIBLE);
+                textView4.setVisibility(view.INVISIBLE);
+                textView5.setVisibility(view.INVISIBLE);
+                textView6.setVisibility(view.INVISIBLE);
+                textView7.setVisibility(view.INVISIBLE);
+                textView.setVisibility(view.VISIBLE);
+                textView1.setVisibility(view.VISIBLE);
+                textView2.setVisibility(view.VISIBLE);
+                textView3.setVisibility(view.VISIBLE);
+
                 etbtn.setVisibility(view.VISIBLE);
+
             }
         });
 
